2e2385ee96e401d488eec791f4e8b3487938ad07
Michael Ho <michael.ho@ieee.org>
2023-01-27 02:28:47 +0100

    inhabitants: add james
    Start developing the first inhabitant for the terrarium. james simply
    lists the pods running in kubernetes for now, sleeps 30s and repeats.
    
    This starting point can be used to start building the containers to then
    start building the helm charts to deploy james.

---
+++ inhabitants/james/agent.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import time
+
+from kubernetes import client, config
+
+
+def main():
+    config.load_incluster_config()
+
+    v1 = client.CoreV1Api()
+
+    while True:
+        print("Listing pods with their IPs:")
+        ret = v1.list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
